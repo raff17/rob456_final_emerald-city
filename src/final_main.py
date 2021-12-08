c8e5a2e0fdfbee1d8c86e200d7ffef4f19760c78
rawiec <95449159+rawiec@users.noreply.github.com>
2021-12-07 21:11:48 -0800

    Add files via upload

--- src/final_main.py
+++ src/final_main.py
@@ -19,7 +19,7 @@ class MainRun:
         self.robot_loc_y = 0
         self.did_send = False  # flag_from_ros
         self.target_pub = rospy.Publisher('/Current_Target', Pose, queue_size=10)
-        print('passed current target line')
+        #print('passed current target line')
         self.pos_sub = rospy.Subscriber('/map_odom', Pose, self.main_run_func)
 
 
@@ -61,21 +61,19 @@ class MainRun:
 
 
     def publish_target(self):
-        print("target published!")
+        #print("target published!")
 	self.target_pub.publish(self.target_pose)
 
     def current_pos_interpret(self, pos_msg):
         self.robot_loc_x = pos_msg.position.x
-        self.robot_loc_x = pos_msg.position.y
-	print([self.robot_loc_x, self.robot_loc_y])
+        self.robot_loc_y = pos_msg.position.y
+	#print([self.robot_loc_x, self.robot_loc_y])
 
     def map_range(self, x, in_min, in_max, out_min, out_max):
         return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 
-print('hi')
 if __name__ == '__main__':
-    print('whattt')
     time_limit = rospy.Duration(2)  # Time allowed for robot to reach destination in ms?
     beg_time = rospy.Duration(0)
     # current_time = 0
